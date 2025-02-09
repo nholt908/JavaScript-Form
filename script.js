de709d9655fe25a1d76316442d0c73e4e21b4826
Nicole <nholt3@atu.edu>
2025-02-08 22:50:17 -0600

    completed form options

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function displayInformation(form){
-    document.getElementById("info").innerHTML = "Name: " + form.elements["name"].value + "<br>";
+    if(!document.getElementById("agree").checked){
+        alert("Please agree to the Terms and Conditions");
+        return;
+    }
+    document.getElementById("info").innerHTML = "Name: " + form.elements["name"].value + "<br>"
+    + "Email: " + form.elements["email"].value + "<br>"
+    + "Password: " + form.elements["password"].value + "<br>"
+    + "Age: " + form.elements["age"].value + "<br>"
+    + "Gender: " + form.elements["gender"].value + "<br>"
+    + "Country: " + form.elements["country"].value;
 }
 
 function hideInformation(){
